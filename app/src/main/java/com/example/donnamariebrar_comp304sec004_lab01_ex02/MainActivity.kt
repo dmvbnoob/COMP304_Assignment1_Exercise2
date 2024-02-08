a74fb837f3b1537ada8b2f228bd828660435b764
DMBrar <dmvbrar@gmail.com>
2024-02-08 13:17:03 -0500

    first commit

---
+++ app/src/main/java/com/example/donnamariebrar_comp304sec004_lab01_ex02/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.donnamariebrar_comp304sec004_lab01_ex02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "donnamariebrar_COMP304Sec004_Lab01_Ex02"
+include(":app")
